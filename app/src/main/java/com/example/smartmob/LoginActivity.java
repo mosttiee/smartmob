77d883386a321375c532ce1740eb439fb417d3bb
Thamonwan Tangchitdee <thamonwan_most@hotmail.com>
2019-11-09 17:49:58 +0700

    Update gitignore Add btn onclick listener and login page.

---
+++ app/src/main/java/com/example/smartmob/LoginActivity.java
@@ -0,0 +1,17 @@
+package com.example.smartmob;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
--- app/src/main/java/com/example/smartmob/MainActivity.java
+++ app/src/main/java/com/example/smartmob/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.smartmob;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
+    public void onLoginBtn(View v){
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_landing);
+
+        final Button button = findViewById(R.id.loginButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                onLoginBtn(v);
+            }
+        });
     }
 }
