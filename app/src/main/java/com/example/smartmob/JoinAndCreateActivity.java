f0bb237397d3c4d086374b172700ff25ed80cd2b
Thamonwan Tangchitdee <thamonwan_most@hotmail.com>
2019-11-10 00:09:42 +0700

    Add btn onclick listener, join page and joinAndCreate page.

--- app/src/main/java/com/example/smartmob/JoinAndCreateActivity.java
+++ app/src/main/java/com/example/smartmob/JoinAndCreateActivity.java
@@ -1,15 +1,29 @@
 package com.example.smartmob;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class JoinAndCreateActivity extends AppCompatActivity {
 
+    public void onJoinBtn(View v){
+        Intent intent = new Intent(this, JoinAndCreateActivity.class);
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join_and_create);
+
+        final Button button = findViewById(R.id.joinNewGroupButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                onJoinBtn(v);
+            }
+        });
     }
 }
