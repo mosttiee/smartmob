89347df5aa63ea4c76e535a46d26370dcfe53cd9
Thamonwan Tangchitdee <thamonwan_most@hotmail.com>
2019-11-10 02:33:15 +0700

    fixed conflicts

--- app/src/main/java/com/example/smartmob/FloodMessageActivity.java
+++ app/src/main/java/com/example/smartmob/FloodMessageActivity.java
@@ -4,14 +4,40 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class FloodMessageActivity extends AppCompatActivity {
 
+    public String type = "";
+
+    public void onRadioButtonClicked(View view) {
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.normal:
+                if (checked)
+                    type = "normal";
+                break;
+            case R.id.secret:
+                if (checked)
+                    type = "secret";
+                break;
+            case R.id.emergency:
+                if (checked)
+                    type = "emergency";
+                break;
+        }
+    }
+
     public void onSubmitBtn(View v){
-        Intent intent = new Intent(this, FloodMessageActivity.class);
-        startActivity(intent);
+        //to check the type of msg
+        onRadioButtonClicked(v) ;
+
+        //do the flooding msg function
+
     }
 
     @Override
